61b7bb2e421081fb581cb32a3099b4df35ed8b46
Clifford Tse <TseClifford@gmail.com>
2022-01-18 21:48:20 +0000

    added new refactored spinner function

---
+++ spinner2.js
@@ -0,0 +1,19 @@
+const lines = ['|', '/', '-', '\\']
+
+const spinner = totalSpins => {
+  let time = 100
+  for (let spins = 0; spins < totalSpins; spins++) {
+    for (let i = 0; i < lines.length; i++) {
+      setTimeout(() => {
+        process.stdout.write(`\r${lines[i]}   `);
+      }, time);
+      time += 200
+    }
+  }
+  // Reset to initial state and force new line
+  setTimeout(() => {
+    process.stdout.write(`\r|   \n`);
+  }, time);
+}
+
+spinner(3)
