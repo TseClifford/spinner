2831d54a24449a166a2f1a7d695bf163d5d6b24a
Clifford Tse <TseClifford@gmail.com>
2022-01-19 02:14:42 +0000

    fixed linting

--- spinner2.js
+++ spinner2.js
@@ -1,19 +1,19 @@
-const lines = ['|', '/', '-', '\\']
+const lines = ['|', '/', '-', '\\'];
 
 const spinner = totalSpins => {
-  let time = 100
+  let time = 100;
   for (let spins = 0; spins < totalSpins; spins++) {
     for (let i = 0; i < lines.length; i++) {
       setTimeout(() => {
         process.stdout.write(`\r${lines[i]}   `);
       }, time);
-      time += 200
+      time += 200;
     }
   }
   // Reset to initial state and force new line
   setTimeout(() => {
     process.stdout.write(`\r|   \n`);
   }, time);
-}
+};
 
-spinner(3)
+spinner(3);
